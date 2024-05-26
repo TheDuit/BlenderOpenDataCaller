efca7545cb364d94b665dd314f8783043a85d015
TheDuit <53624486+TheDuit@users.noreply.github.com>
2024-05-25 21:49:39 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route('/api/blenderData', methods=['GET'])
+def get_blender_data():
+    blender_api_url = "https://opendata.blender.org/benchmarks/query/?group_by=device_name&group_by=compute_type&response_type=datatables"
+    
+    try:
+        response = requests.get(blender_api_url)
+        
+        if response.status_code == 200:
+            return jsonify(response.json())
+        else:
+            return jsonify({"error": f"Failed to fetch data from Blender API. Status code: {response.status_code}"})
+    except Exception as e:
+        return jsonify({"error": f"An error occurred: {str(e)}"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
